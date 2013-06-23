7c980ce7507fd5c24c9bcd1634fb3b182d1d28e9
Pablo Sanfilippo <giamgreeg@gmail.com>
2013-06-22 22:35:28 -0300

    Fixed #2 y #3. Se puede salir del programa de forma civilizada y el código está más prolijo. Agregado también el include de stdlib.h para eliminar los avisos de gcc.

--- lanalyzer.c
+++ lanalyzer.c
@@ -18,32 +18,16 @@ char estados_aceptacion[ESTADOS_MAX];
 
 // Prototipos
 // ==========
-int existe_estado(char estado); //Devuelve 1 si el estado ya existe en la tabla, else 0.
-int existe_simbolo (char simbolo);//Devuelve 1 si el simbolo ya existe en la tabla, else 0.
 void cargar_tabla();
 char move(char, char);
-int match (char *cadena);
+int match (char*);
 char leer_char();
-int donde_guardar_move(char estado, char simbolo);
-int donde_guardar_transicion(char estado);
+int donde_guardar_move(char, char);
+int donde_guardar_transicion(char);
 
 
 // Funciones	
 // =========
-
-void print_tabla() {
-	int i, j;
-	printf("Printing tabla.\n");
-	printf("======================\n");
-	for (i = 0; i < ESTADOS_MAX; ++i)
-	{
-		for (j = 0; j < SIMBOLOS_MAX; ++j)
-		{
-			printf("De %c a %c a través de %c.\n", tabla[i].estado_origen, tabla[i].move[j][1], tabla[i].move[j][0]);
-		}
-	}
-	printf("======================\n");
-}
 
 char leer_char() {
 	/* Devuelve un caracter leído por teclado.
@@ -55,43 +39,40 @@ char leer_char() {
 }
 
 void cargar_tabla() {
+	/* Procedimiento general de carga de datos sobre el AFD. */
 	int i = 0, j = 0, indice_tabla = 0, indice_move;
-	char estado,simbolo,estado_destino;
+	char estado, simbolo, estado_destino;
 	
-	//Llenar todos los campos de la tabla con un valor convención, tipo '*'. Va a servir.
-	for (i=0;i<ESTADOS_MAX; i++){
+	// Llenar todos los campos de la tabla con un valor convención, '*'.
+	for (i = 0; i < ESTADOS_MAX; i++) {
 		tabla[i].estado_origen  = '*';
-		for (j=0;j<SIMBOLOS_MAX;j++){
-			tabla[i].move[j][0]= '*';
-			tabla[i].move[j][1]= '*';
+		for (j = 0; j < SIMBOLOS_MAX; j++){
+			tabla[i].move[j][0] = '*';
+			tabla[i].move[j][1] = '*';
 		}
 	}
+
+	//Llenar con * también los campos del array estados_aceptación.
 	for (i = 0; i < ESTADOS_MAX; ++i) {
 		estados_aceptacion[i] = '*';
 	}
 
-	print_tabla();
-	//Ahora si, a cargar la tabla
+	// Cargar los datos en la tabla.
+	printf("Cargar datos del AFD.\n");
 	while (1) {
 		
-		printf("Ingresar estado origen: ");
+		printf("Ingresar estado origen (Enter para terminar): ");
 		estado = leer_char();
 		if (estado == '\n') break;
 		indice_tabla = donde_guardar_transicion(estado);
-		
-		print_tabla();
 
 		printf("Ingresar simbolo: ");
 		simbolo = leer_char();
 		indice_move = donde_guardar_move(indice_tabla, simbolo);
 		tabla[indice_tabla].move[indice_move][0] = simbolo;
-
-		print_tabla();
 		
 		printf("Ingresar estado destino: ");
 		tabla[indice_tabla].move[indice_move][1] = leer_char();
-
-		print_tabla();
 	}
 
 	printf("Ingresar estado_inicial: ");
@@ -120,10 +101,7 @@ int donde_guardar_move(char estado, char simbolo) {
 		}
 		i++;
 	}
-	if (i > SIMBOLOS_MAX) {
-		/* No hay lugar para más símbolos. */
-		exit(1);
-	}
+
 	tabla[estado].move[i][0] = simbolo;
 	return i;
 }
@@ -138,10 +116,7 @@ int donde_guardar_transicion(char estado) {
     	if (tabla[i].estado_origen == estado) return i;
     	i++;
    }
-   if (i > ESTADOS_MAX) {
-		/* No hay lugar para más estados. */
-		exit(1);
-	}
+
 	tabla[i].estado_origen = estado;
 	return i;
 }
@@ -150,23 +125,16 @@ char move(char estado, char simbolo) {
 	/* Toma un estado y un símbolo y devuelve el estado
 	al cual se llega desde ese estado a través de ese
 	símbolo. Si esto no es posible, devuelve '*'. */
-	printf("[DEBUG] Buscando en la tabla el estado %c.\n", estado);
 	int i_estado = 0, i_simbolo = 0, i = 0;
 	while (tabla[i].estado_origen != '*' && i <= ESTADOS_MAX) {
     	if (tabla[i].estado_origen == estado) break;
     	i++;
    	}
-	if (i > ESTADOS_MAX || tabla[i].estado_origen == '*') {
-		/* Error, ese estado no existe. */
-		exit(1);
-	}
+
 	i_estado = i;
-	printf("[DEBUG] Encontrado. %d.\n", i_estado);
-	printf("[DEBUG] Buscando en esa transicion el simbolo %c.\n", simbolo);
 	while (tabla[i_estado].move[i_simbolo][0] != '*' && i <= SIMBOLOS_MAX) {
 		if (tabla[i_estado].move[i_simbolo][0] == simbolo) {
 			//Ya tengo el i_estado y i_simbolo, devolver estado destino.
-			printf("[DEBUG] Encontrado. %d.\n", i_simbolo);
 			return tabla[i_estado].move[i_simbolo][1];
 		}
 		i++;
@@ -178,16 +146,11 @@ char move(char estado, char simbolo) {
 }
 
 int match (char *cadena) { //Devuelve 1 si la cadena pasada coincide con el AFD.
-	printf("[DEBUG] Iniciando match.\n");
 	int i = 0;
 	char estado_actual;
-	printf("[DEBUG] estado_inicial = %c.\n", estado_inicial);
-	printf("[DEBUG] Intentando transicion a través del simbolo %c.\n", cadena[0]);
 	estado_actual = move(estado_inicial, cadena [0]);
 	if (estado_actual == '*') return 0; //El asterisco es una convención.
 	for (i = 1; i < strlen(cadena); i++) {
-		printf("[DEBUG] estado_actual = %c.\n", estado_actual);
-		printf("[DEBUG] Intentando transicion a través del simbolo %c.\n", cadena[i]);
 		estado_actual = move(estado_actual, cadena[i]);
 		if (estado_actual == '*') return 0;
 	}
@@ -208,9 +171,9 @@ int main() {
 	char cadena[100];
 	cargar_tabla();
 	while (1) {
-		printf("Ingresar cadena a analizar: ");
+		printf("Ingresar cadena a analizar (* para salir): ");
 		scanf("%s", &cadena);
-		if (cadena[0] == '\0') return 0;
+		if (cadena[0] == '*') return 0;
 		if ( match(cadena) ) printf("La cadena %s coincide.\n", cadena);
 		else printf("La cadena %s no coincide. \n", cadena);
 	}
